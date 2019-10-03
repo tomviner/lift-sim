87e7faaeb1014fb04a85c3cf1fd22cf621277ed9
jenjnif <jenjnif@hotmail.com>
2019-10-03 20:45:43 +0100

    Update lift.py

--- lift.py
+++ lift.py
@@ -1,11 +1,29 @@
+import random
+
+
 class Lift():
 
-    def __init__(self, num_floors):
-        self.num_floors = num_floors
+    def __init__(self, display):
+        self.num_floors = display.floors
         self.floors = []
+        self.display = display
+        self.going_up = 1
+        self.floor = 0
 
     def next_floor(self):
-        return self.floors.pop(0)
+        while 1:
+            yield self.floor
+            if self.floors:
+                self.floor = self.floors.pop(0)
+            else:
+                self.floor = random.randint(self.num_floors)
 
     def requested_floor(self, floor):
         self.floors.append(floor)
+
+    def start(self):
+        for floor in self.next_floor():
+            self.display.send_lift_to(floor)
+
+    def closest_floor:
+        pass
